44f544e141f782a0687bbfc8baf89134fd157b6a
Svitlan Kueng <79264553+Svity13@users.noreply.github.com>
2026-02-04 12:37:40 +0100

    Update game.py
    Changed a typo

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import random
 
 def mainMenu():
-    print("Welcome to the Number Guessing Game\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number.\n")
+    print("Welcome to the Number Guessing Game\nI'm thinking of a number between 1 and 50.\nYou have 5 chances to guess the correct number.\n")
     print("Please select the difficulty level:\n1. Easy (10 chances)\n2. Medium (5 chances)\n3. Hard (3 chances)")
     option = int(input("Enter your choice: "))
     if option == 1:
